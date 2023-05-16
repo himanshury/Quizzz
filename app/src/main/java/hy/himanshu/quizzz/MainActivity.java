bc29515aae04b28778799c95f8400ed2068b17d7
Himanshu <himanshury1231@gmail.com>
2023-05-17 04:07:12 +0530

    Score Sheet Added

--- app/src/main/java/hy/himanshu/quizzz/MainActivity.java
+++ app/src/main/java/hy/himanshu/quizzz/MainActivity.java
@@ -3,9 +3,14 @@ package hy.himanshu.quizzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.color.utilities.Score;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -95,14 +100,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void showSheet()
+    {
+        BottomSheetDialog bottomSheetDialog=new BottomSheetDialog(MainActivity.this);
+        View bottomSheetVIew= LayoutInflater.from(getApplicationContext()).inflate(R.layout.score_sheet,(LinearLayout)findViewById(R.id.idLLScore));
+        TextView ScoreTV= bottomSheetVIew.findViewById(R.id.Score);
+        Button restart= bottomSheetVIew.findViewById(R.id.Restart);
+        ScoreTV.setText("Your Score is \n"+currentscore+"/4");
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPos= random.nextInt(quizModelArrayList.size());
+                setDataToViews(currentPos);
+                questionAttempted=1;
+                currentscore=0;
+                bottomSheetDialog.dismiss();
+            }
+        });
+      bottomSheetDialog.setCancelable(false);
+      bottomSheetDialog.setContentView(bottomSheetVIew);
+      bottomSheetDialog.show();
+    }
+
+
     private void setDataToViews(int currentPos) {
 
             questionNumber.setText("Questions Attempted: "+questionAttempted + "/4");
-            question.setText(quizModelArrayList.get(currentPos).getQuestion());
-            opt1.setText(quizModelArrayList.get(currentPos).getOption1());
-            opt2.setText(quizModelArrayList.get(currentPos).getOption2());
-            opt3.setText(quizModelArrayList.get(currentPos).getOption3());
-            opt4.setText(quizModelArrayList.get(currentPos).getOption4());
+            if(questionAttempted==4)
+            {
+                showSheet();
+            }
+            else
+            {
+                question.setText(quizModelArrayList.get(currentPos).getQuestion());
+                opt1.setText(quizModelArrayList.get(currentPos).getOption1());
+                opt2.setText(quizModelArrayList.get(currentPos).getOption2());
+                opt3.setText(quizModelArrayList.get(currentPos).getOption3());
+                opt4.setText(quizModelArrayList.get(currentPos).getOption4());
+            }
+
 
 
     }
